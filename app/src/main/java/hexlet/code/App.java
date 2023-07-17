73f20da113f05aaa02504aa071f1c6af0ff6f8d0
Leonid <l3on102@gmail.com>
2023-07-17 23:37:08 +0900

    added scanner for change game in App.main

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
 
 
 public class App {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        int userSelected = scanner.nextInt();
+        System.out.println("Your choice: " + userSelected);
+        //System.out.println("Welcome to the Brain Games!");
+        if (userSelected == 0) {
+            scanner.close();
+        } else if (userSelected == 1) {
+            Cli.newUser();
+        }
 
-        System.out.println("Welcome to the Brain Games!");
-        Cli.newUser();
     }
 }
