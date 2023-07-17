5f610e0e6de55a8f809657cd01df68c225fde42d
Leonid <l3on102@gmail.com>
2023-07-17 22:22:12 +0900

    added changes makefile added Cli class added checkstyle.xml added bage codeclimate to README.md

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void newUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+
+    }
+
+}
+
